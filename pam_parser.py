f65a143435c0ef65cde935af8a4696720dd8e4b5
Mihir Manna <mihirmanna2@gmail.com>
2023-08-08 09:15:05 -0500

    Add rules_list option instead of always printing the rules list

--- pam_parser.py
+++ pam_parser.py
@@ -6,34 +6,42 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='This tool parses PAM files into human-readable formats, enabling '
                                                  'systems admins to more easily debug them.')
 
-    # Required arguments
+    # Positional arguments
     parser.add_argument(
         'config_file',
         type=str,
         help='The path to the PAM config file')
 
-    # Optional arguments
-    parser.add_argument(
-        '-t', '--module_type',
-        type=str,
-        help='Only modules of this type are included in the stack outputs. '
-             'Must be one of: auth, account, password, session')
+    # Options
     parser.add_argument(
         '-m', '--mermaid',
         default=False,
         action='store_true',
-        help='Return a URL to the Mermaid flowchart')
+        help='Return a URL to the Mermaid flowchart'
+    )
+    parser.add_argument(
+        '-r', '--rules_list',
+        default=False,
+        action='store_true',
+        help='Return a formatted list of all rules in the PAM config file'
+    )
     parser.add_argument(
         '-s', '--text_stacks',
         default=False,
         action='store_true',
-        help='Return a list of all possible stacks')
+        help='Return a list of all possible stacks'
+    )
+    parser.add_argument(
+        '-t', '--module_type',
+        type=str,
+        help='Only modules of this type are included in the stack outputs. '
+             'Must be one of: auth, account, password, session'
+    )
 
     args = parser.parse_args()
     config_list = read_config(args.config_file)
     rules = parse_rules(config_list)
 
-    print_formatted_config(rules)
     if args.mermaid is True:
         if args.module_type is None:
             parser.error('The --mermaid (-m) argument requires --module_type (-t) MODULE_TYPE')
@@ -45,3 +53,5 @@ if __name__ == '__main__':
         else:
             stacks = generate_stacks(rules, args.module_type, [[]])
             print_formatted_stacks(stacks)
+    if args.rules_list is True:
+        print_formatted_config(rules)
